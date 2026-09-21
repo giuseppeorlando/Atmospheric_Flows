7d7bdb2269d994366340da7d66b286e0acc7eb10
Giuseppe Orlando <giuseppe1.orlando@mail.polimi.it>
2026-09-21 17:37:52 +0200

    Minor modifications to helper messages

--- TURBULENT_ITERATIVE_SOLVERS/Atmospheric_Flow_turbulent.cc
+++ TURBULENT_ITERATIVE_SOLVERS/Atmospheric_Flow_turbulent.cc
@@ -1853,6 +1853,9 @@ int main(int argc, char *argv[]) {
         }
         parameter_file = argv[++i];
       }
+      else if(arg == "--help_test_case") {
+        continue;
+      }
       else {
         std::cerr << "Unknown option: " << arg << "\n";
         print_help(argv[0]);
